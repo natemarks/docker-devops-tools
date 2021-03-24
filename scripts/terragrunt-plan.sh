4f23c58874db9a881bf0f1a298ada76e01dadccb
Nate Marks <npmarks@gmail.com>
2021-03-24 06:43:43 -0400

    fix: troubleshooting keyscan part of terragrunt-plan.sh

--- scripts/terragrunt-plan.sh
+++ scripts/terragrunt-plan.sh
@@ -5,9 +5,11 @@ set -Eeuoxv pipefail
 project_dir="${1}" || exit
 
 chmod 400 /root/.ssh/id_*
-ls -la /root/.ssh
 # add keys to known host insided docker
-ssh -vvv -T -oStrictHostKeyChecking=no stash.imprivata.com -p 7999
-ssh -vvv -T -oStrictHostKeyChecking=no github.com
+ssh-keyscan stash.imprivata.com -p 7999 > /root/.ssh/known_hosts || true
+ssh-keyscan github.com >> /root/.ssh/known_hosts || true
+chmod 644 /root/.ssh/known_hosts
+ls -la /root/.ssh
+cat /root/.ssh/known_hosts
 cd "${project_dir}"
 terragrunt init && terragrunt plan
