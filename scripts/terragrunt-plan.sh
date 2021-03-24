d0267d6f917f1fbfbb071b8c703c4b94bebc9a38
Nate Marks <npmarks@gmail.com>
2021-03-24 06:12:52 -0400

    fix: Add ls for /root/.ssh dir for troubleshooting

--- scripts/terragrunt-plan.sh
+++ scripts/terragrunt-plan.sh
@@ -5,6 +5,7 @@ set -Eeuoxv pipefail
 project_dir="${1}" || exit
 
 chmod 400 /root/.ssh/id_*
+ls -la /root/.ssh
 # add keys to known host insided docker
 ssh -vvv -T -oStrictHostKeyChecking=no stash.imprivata.com -p 7999
 ssh -vvv -T -oStrictHostKeyChecking=no github.com
