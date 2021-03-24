235c93ef9d0725c902b620ad8d20a7a88d32d132
Nate Marks <npmarks@gmail.com>
2021-03-24 06:10:40 -0400

    fix: Add ssh debug logging to terragrunt-plan.sh

--- scripts/terragrunt-plan.sh
+++ scripts/terragrunt-plan.sh
@@ -6,7 +6,7 @@ project_dir="${1}" || exit
 
 chmod 400 /root/.ssh/id_*
 # add keys to known host insided docker
-ssh -T -oStrictHostKeyChecking=no stash.imprivata.com -p 7999
-ssh -T -oStrictHostKeyChecking=no github.com
+ssh -vvv -T -oStrictHostKeyChecking=no stash.imprivata.com -p 7999
+ssh -vvv -T -oStrictHostKeyChecking=no github.com
 cd "${project_dir}"
 terragrunt init && terragrunt plan
