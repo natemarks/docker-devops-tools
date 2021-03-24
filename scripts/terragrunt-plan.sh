15c169b38a9b6bce08ebc3d7be38a10f986fa45e
Nate Marks <npmarks@gmail.com>
2021-03-24 09:37:44 -0400

    fix: keyscan -p has to be before the host

--- scripts/terragrunt-plan.sh
+++ scripts/terragrunt-plan.sh
@@ -6,7 +6,7 @@ project_dir="${1}" || exit
 
 chmod 400 /root/.ssh/id_*
 # add keys to known host insided docker
-ssh-keyscan stash.imprivata.com -p 7999 > /root/.ssh/known_hosts || true
+ssh-keyscan -p 7999 stash.imprivata.com > /root/.ssh/known_hosts || true
 ssh-keyscan github.com >> /root/.ssh/known_hosts || true
 chmod 644 /root/.ssh/known_hosts
 ls -la /root/.ssh
