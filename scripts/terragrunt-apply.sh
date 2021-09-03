b2660c1a3ee2826217cfda837554bf8490023284
Nate Marks <npmarks@gmail.com>
2021-09-03 04:40:58 -0400

    fix: Fix ssh error in scripts/terragrunt-apply.sh

--- scripts/terragrunt-apply.sh
+++ scripts/terragrunt-apply.sh
@@ -6,7 +6,10 @@ project_dir="${1}" || exit
 
 chmod 400 /root/.ssh/id_*
 # add keys to known host insided docker
-ssh -T -oStrictHostKeyChecking=no stash.imprivata.com -p 7999
-ssh -T -oStrictHostKeyChecking=no github.com
+ssh-keyscan -p 7999 stash.imprivata.com > /root/.ssh/known_hosts || true
+ssh-keyscan github.com >> /root/.ssh/known_hosts || true
+chmod 644 /root/.ssh/known_hosts
+ls -la /root/.ssh
+cat /root/.ssh/known_hosts
 cd "${project_dir}"
 terragrunt init && terragrunt apply --terragrunt-non-interactive
